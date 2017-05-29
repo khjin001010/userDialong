65f0512c72459c6716e338387d225186947c308e
khjin001010 <khjin001010@naver.com>
2017-05-29 20:54:58 +0900

    #2 make reference

--- app/src/main/java/khjin001010/kr/hs/emirim/userdialong/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/userdialong/MainActivity.java
@@ -1,13 +1,33 @@
 package khjin001010.kr.hs.emirim.userdialong;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but = (Button)findViewById(R.id.but);
+
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        View dlgV = View.inflate(this, R.layout.dialog, null); //getApplicationContext == this
+        //inflation == xml에 있는 것을 가지고 객체 생성하는 것.
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        //getApplicationContext == this
+        dialog.setTitle("Input User Information");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        dialog.setView(dlgV);
+        dialog.setPositiveButton("OK",null);
+        dialog.setNegativeButton("CANCEL",null);
+        dialog.show();
     }
 }
